b1518996821d5b0da18dad869ff66e168a9c86e7
DaniloSalerno <salerno.danilo@hotmail.it>
2023-11-17 15:00:52 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <h1 class="red">
+    Hello vue
+  </h1>
+</template>
+
+<style scoped>
+.red {
+  color: red;
+}
+</style>
