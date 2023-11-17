b4603346ccf091aa5863a9688444b9a98f473c9a
DaniloSalerno <salerno.danilo@hotmail.it>
2023-11-17 15:22:32 +0100

    milestone 3&4
    add getProjects() in methods and recovery all projects from API, added cards with project info in templete by v-for directive

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <script>
+import axios from 'axios';
 export default {
   name: 'App',
   data() {
     return {
-
+      base_url: 'http://127.0.0.1:8000',
+      projects_api: '/api/projects',
+      projects: [],
     }
+
+  },
+  methods: {
+    getProjects() {
+      axios
+        .get(this.base_url + this.projects_api)
+        .then(response => {
+          console.log(response);
+          this.projects = response.data.result
+        })
+        .catch(error => {
+          console.error(error);
+        })
+    }
+  },
+  mounted() {
+    this.getProjects()
   }
 }
 </script>
 
 <template>
-  <h1 class="red">
-    Hello vue
-  </h1>
+  <div class="container">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5">
+      <div class="col" v-for="project in projects.data">
+        <div class="card">
+
+          <img :src="project.thumb" alt="">
+
+          <h3>{{ project.title }}</h3>
+
+          <div>{{ project.description }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-.red {
-  color: red;
-}
-</style>
+<style scoped></style>
