cf61b4e6259ca097d42f4b4e34916f84d68a6378
DaniloSalerno <salerno.danilo@hotmail.it>
2023-11-20 17:15:00 +0100

    add the 3 latest project in HomeView component

--- src/state.js
+++ src/state.js
@@ -5,7 +5,9 @@ export const state = reactive({
 
     base_url: 'http://127.0.0.1:8000',
     projects_api: '/api/projects',
+    latestProject_api: '/api/projects-latest',
     projects: [],
+    latestProjects: [],
     api_page: 1,
     project: {},
     type: '',
@@ -21,6 +23,18 @@ export const state = reactive({
             .then(response => {
                 //console.log(response);
                 this.projects = response.data.result
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    },
+
+    getLatestProjects() {
+        axios
+            .get(this.base_url + this.latestProject_api)
+            .then(response => {
+                console.log(response);
+                this.latestProjects = response.data.result
             })
             .catch(error => {
                 console.error(error);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,35 @@
 <script>
+import ProjectCard from '../components/ProjectCard.vue';
+
+import { state } from '../state.js';
+
 export default {
-    name: 'HomeView'
+    name: 'HomeView',
+    data() {
+        return {
+            state
+        }
+
+    },
+    components: {
+        ProjectCard
+    },
+    mounted() {
+        this.state.getLatestProjects()
+    }
 }
 </script>
 
 <template>
-    <div>
-        <h1>Hello Home</h1>
+    <section class="latest py-5">
+        <div class="container">
+            <h2 class="text-uppercase">My latest projects</h2>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+                <ProjectCard :project="project" v-for="project in this.state.latestProjects" />
+            </div>
+        </div>
 
-    </div>
+    </section>
 </template>
 
 
