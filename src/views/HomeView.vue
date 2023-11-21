3c7fc88f496dc5216adda0a36604e5fc2b19fb76
DaniloSalerno <salerno.danilo@hotmail.it>
2023-11-21 15:22:54 +0100

    add placeholder cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,11 +22,34 @@ export default {
 
 <template>
     <section class="latest py-5">
-        <div class="container">
+        <div class="container" v-if="this.state.loading">
             <h2 class="text-uppercase">My latest projects</h2>
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
                 <ProjectCard :project="project" v-for="project in this.state.latestProjects" />
             </div>
+        </div>
+
+        <div v-else>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4 py-5">
+                <div class="col" v-for="n in 3">
+                    <div class="card" aria-hidden="true">
+                        <div class="card-body">
+                            <h5 class="card-title placeholder-glow">
+                                <span class="placeholder col-6"></span>
+                            </h5>
+                            <p class="card-text placeholder-glow">
+                                <span class="placeholder col-7"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-6"></span>
+                                <span class="placeholder col-8"></span>
+                            </p>
+                            <a class="btn btn-secondary w-100 disabled placeholder col-6" aria-disabled="true"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
     </section>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -11,7 +11,7 @@ export default {
 
             projects: [],
 
-            loading: null,
+            loading: false,
 
             links: [],
 
@@ -32,6 +32,7 @@ export default {
                     this.projects = response.data.result.data;
                     this.lastPage = response.data.result.last_page;
                     this.links = response.data.result.links;
+                    this.loading = true;
                     console.log(this.projects);
 
                 })
@@ -48,26 +49,52 @@ export default {
 
 <template>
     <div class="container">
-        <h1 class="py-5">Projects</h1>
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-4">
+        <div v-if="this.loading">
+            <h1 class="py-5">Projects</h1>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-4">
 
-            <ProjectCard :project="project" v-for="project in this.projects" />
+                <ProjectCard :project="project" v-for="project in this.projects" />
 
+
+            </div>
+
+            <div class="py-5">
+
+                <nav aria-label="Page navigation" class="d-flex justify-content-center">
+                    <ul class="pagination">
+                        <li class="page-item" :class="link.active ? 'active' : ''" aria-current="page"
+                            v-for="link in this.links">
+                            <a class="page-link" role="button" @click="this.getProjects(link.url)" v-html="link.label"></a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
+
+        <div v-else>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-4 py-5">
+                <div class="col" v-for="n in 10">
+                    <div class="card" aria-hidden="true">
+                        <div class="card-body">
+                            <h5 class="card-title placeholder-glow">
+                                <span class="placeholder col-6"></span>
+                            </h5>
+                            <p class="card-text placeholder-glow">
+                                <span class="placeholder col-7"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-6"></span>
+                                <span class="placeholder col-8"></span>
+                            </p>
+                            <a class="btn btn-secondary w-100 disabled placeholder col-6" aria-disabled="true"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
 
-        <div class="py-5" v-if="!this.loading">
-
-            <nav aria-label="Page navigation" class="d-flex justify-content-center">
-                <ul class="pagination">
-                    <li class="page-item" :class="link.active ? 'active' : ''" aria-current="page"
-                        v-for="link in this.links">
-                        <a class="page-link" role="button" @click="this.getProjects(link.url)" v-html="link.label"></a>
-                    </li>
-                </ul>
-            </nav>
-
-        </div>
     </div>
 </template>
 
